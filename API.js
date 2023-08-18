d2f44bec8639812c84745fd12dae3557280f5ecf
Euro Rincon Garrido <134429160+rinconeuro@users.noreply.github.com>
2023-08-18 18:43:53 +0000

    sw

--- API.js
+++ API.js
@@ -1,4 +1,4 @@
 fetch("https://swapi.tech/api/people/1")
 .then(res => res.json())
-.then(data => console.log(data.result.properties))
+.then(data => console.log(data.result.properties.name))
 .catch(err => console.log(err))
