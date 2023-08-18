7406450e398c1a72f4d14be1a6f61d8791fd4a86
Rémulo Castro <103341303+Remug@users.noreply.github.com>
2023-08-18 19:51:22 +0000

    rem

--- Character-StarWars/charactergenerator.js
+++ Character-StarWars/charactergenerator.js
@@ -3,13 +3,14 @@ console.log('Hola')
 function setChar() {
     fetch("https://swapi.tech/api/people/1")
     .then(res => res.json())
-    .then(data => console.log(data.result.properties.name))
+    .then(data => {
+        const characterName = data.result.properties.name
+
+        const h1Element = document.getElementById('name');
+        h1Element.innerHTML = characterName;
+    })
     .catch(err => console.log(err))
 
-    return
 }
-
-const genChar = document.getElementById ('name').innerHTML = 
-
-
-    
+   
+setChar();
